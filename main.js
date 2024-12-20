7d2939d3091c438280c017b6ce4074b23e0c021e
Edanur Korkmaz <edanuurkorkmaz@gmail.com>
2024-12-20 14:04:34 +0300

    sayilarin karelerini alan uygulama yapildi.

---
+++ main.js
@@ -0,0 +1,21 @@
+/* Bir dizideki sayıların karesini alıp yeni bir dizi döndüren bir fonksiyon yazın.
+
+Adımlar:
+
+1. `map` kullanılarak dizinin her elemanı işlenir.
+
+2. Her eleman kendisiyle çarpılır.
+
+3. Yeni dizi döndürülür.
+
+4.Oluşan dizi consola yazdırın */
+
+const numbers = [1,2,3,4,5,6,7,8];
+const squaredNumbers = numbers.map(twoSquare);
+
+function twoSquare(value, index, array){
+    return value * value;
+}
+
+console.log(numbers);
+console.log(squaredNumbers);
